fa8ff561f679ef8e8bfda074dc7fff9f8dd04083
YG <1610098853@qq.com>
2017-07-03 18:14:20 +0800

    drage

---
+++ app/src/main/java/home/mymodel/LSP/DrageTest.java
@@ -0,0 +1,18 @@
+package MyView.DrageView;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import newhome.baselibrary.R;
+
+/**
+ * Created by Administrator on 2017/7/3 0003.
+ */
+
+public class DrageTest extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_drageview);
+    }
+}
